31ff8dbf233df03172db617f915fde9c370c3fce
金维纲 <jinweigang@meituan.com>
2016-03-23 19:56:27 +0800

    validation 添加demo

--- js/validation.js
+++ js/validation.js
@@ -1,44 +1,75 @@
-/**
- * author: jinweigang
+/***********************************************************************
+ * author: 798757301@qq.com
  * date: 2016-03-22
- * description 该控件主要给红包配置后台提供input校验
- * 使用规则：input标签添加 data-valid={type:Len, min:2, max:10}
+ * description 该控件主要提供input校验
+ * *********************************************************************
+ * eg: input标签添加 data-valid={type:'validNull', text:'xxx不能为空'} | 校验是否为空
+ * eg: input标签添加 data-valid={type:'validLen', min:2, max:10} | 校验长度
  */
 
-define(['../lib/jquery'], function($) {
+define(function() {
   'use strict';
 
   //正则集合
   var REG = {
-    en: /^[a-zA-Z]$/,
-    num: /^[1-9]\d*$/,
-    phone: /^1(3|4|5|7|8)[\d]{9}$/
+    num:   '/^[1-9]\d*$/',
+    en:    '/^[A-Za-z]+$/',
+    cn:    '/^[\u4e00-\u9fa5]{0,}$/',
+    phone: '/^1(3|4|5|7|8)[\d]{9}$/'
   };
 
-  //添加提示
-  var addError = function(input, tip) {
-    var parent = input.parent().css('position', 'relative').addClass('error');
-    var html = '<div class="error-tip">' + tip +'</div>';
-    var errorTip = parent.find('.error-tip');
+  //操作函数
+  var action = {
+    /**
+     * @function addError
+     * @description 给input添加错误提示
+     */
+    addError: function(input, tip) {
+      var parent = input.parent().css('position', 'relative').addClass('valid-error');
+      var html = '<div class="error-tip">' + tip +'</div>';
+      var errorTip = parent.find('.error-tip');
 
-    if(errorTip.length) {errorTip.remove();}
-    input.addClass('error-border');
-    parent.append(html);
-  };
-
-  //是否为空
-  var isNull = function(self, errorText){
-    var flag = false;
-    var value = $.trim(self.val());
-    if (value === '') {
-      self.addClass('error-border');
-      addError(self, errorText || '输入不能为空');
-      flag = true;
-    } else {
-      self.parent().removeClass('error');
-      self.siblings('.error-tip').remove();
+      if(errorTip.length) {errorTip.remove();}
+      input.addClass('error-border');
+      parent.append(html);
+    },
+    /**
+     * @function isNull
+     * @description 判断input值是否为空
+     */
+    isNull: function(self, errorText){
+      var flag = false;
+      var value = $.trim(self.val());
+      if (value === '') {
+        self.addClass('error-border');
+        action.addError(self, errorText || '输入不能为空');
+        flag = true;
+      } else {
+        self.parent().removeClass('valid-error');
+        self.siblings('.error-tip').remove();
+      }
+      return flag;
+    },
+    /**
+     * @function common
+     * @description 各种类型校验公共函数
+     * @param {self} input对象
+     * @param {reg} 校验规则
+     * @param {errorText} 错误文案
+     * @param {defaultText} 默认文案
+     */
+    common: function(self, reg, errorText, defaultText){
+      var value = $.trim(self.val()),
+          pattern = new RegExp(reg);
+      if (action.isNull(self)) return;
+      if (!pattern.test(value)) {
+        self.addClass('error-border');
+        action.addError(self, errorText || defaultText);
+      } else {
+        self.parent().removeClass('valid-error');
+        self.siblings('.error-tip').remove();
+      }
     }
-    return flag;
   };
 
   $.fn.extend({
@@ -50,7 +81,7 @@ define(['../lib/jquery'], function($) {
     validNull: function(errorText) {
       var self = $(this);
       self.on('blur',function() {
-        isNull(self,errorText);
+        action.isNull(self,errorText);
       });
     },
 
@@ -65,15 +96,8 @@ define(['../lib/jquery'], function($) {
       var self = $(this);
       self.on('blur',function() {
         var value = self.val(),
-          len = $.trim(value).length;
-        if (isNull(self)) return;
-        if ((len > max) || (len < min)) {
-          self.addClass('error-border');
-          addError(self, '字符长度应为' + min + '-' + max);
-        } else {
-          self.parent().removeClass('error');
-          self.siblings('.error-tip').remove();
-        }
+            reg = ['/^\S{', min, ',', max,'}$/'].join('');
+        action.common(self, reg, false, '字符长度应为' + min + '~' + max)
       });
     },
 
@@ -85,15 +109,19 @@ define(['../lib/jquery'], function($) {
     validEn: function(errorText) {
       var self = $(this);
       self.on('blur',function() {
-        var value = self.val();
-        if (isNull(self)) return;
-        if (!REG.en.test(value)) {
-          self.addClass('error-border');
-          addError(self, errorText || '请输入英文字母');
-        } else {
-          self.parent().removeClass('error');
-          self.siblings('.error-tip').remove();
-        }
+        action.common(self, REG.en, errorText, '请输入英文字母');
+      });
+    },
+
+    /**
+     * @function validCn
+     * @description 校验英文字母输入
+     * @param {text} 报错文案
+     */
+    validCn: function(errorText) {
+      var self = $(this);
+      self.on('blur',function() {
+        action.common(self, REG.en, errorText, '请输入中文');
       });
     },
 
@@ -105,15 +133,7 @@ define(['../lib/jquery'], function($) {
     validNum: function(errorText) {
       var self = $(this);
       self.on('blur',function() {
-        var value = self.val();
-        if (isNull(self)) return;
-        if (!REG.num.test(value)) {
-          self.addClass('error-border');
-          addError(self, errorText || '请输入数字');
-        } else {
-          self.parent().removeClass('error');
-          self.siblings('.error-tip').remove();
-        }
+        action.common(self, REG.num, errorText, '请输入数字');
       });
     },
 
@@ -125,88 +145,70 @@ define(['../lib/jquery'], function($) {
     validPhone: function(errorText) {
       var self = $(this);
       self.on('blur',function() {
-        var value = self.val();
-        if (isNull(self)) return;
-        if (!REG.phone.test(value)) {
-          self.addClass('error-border');
-          addError(self, errorText || '电话号码不合法');
-        } else {
-          self.parent().removeClass('error');
-          self.siblings('.error-tip').remove();
-        }
+        action.common(self, REG.phone, errorText, '电话号码不合法');
       });
     }
   });
 
-  /**
-   * @function register
-   * @description 外部扩展，自定义注册校验规则
-   * @param {option: validType} 校验类型
-   * @param {option: reg} 校验规则
-   */
-  var register = function(option){
-    var inputList = $('input[data-valid]');
-    inputList.map(function(index, ele) {
-      var self = $(ele),
-          data = eval('(' + self.data('valid') + ')'),
-          validType = data.type;
+  //暴露外部调用方法
+  var valid = {
+    /**
+     * @function register
+     * @description 外部扩展，自定义注册校验规则
+     * @param {option: validType} 校验类型
+     * @param {option: reg} 校验规则
+     */
+    register: function(option){
+      var inputList = $('input[data-valid]');
+      inputList.map(function(index, ele) {
+        var self = $(ele),
+            data = eval('(' + self.data('valid') + ')'),
+            validType = data.type;
 
-      if (option.type != validType) return;
-      self.on('blur',function() {
-        var value = self.val();
-        if (isNull(self)) return;
-        if (!option.reg.test(value)) {
-          self.addClass('error-border');
-          addError(self, data.text);
-        } else {
-          self.parent().removeClass('error');
-          self.siblings('.error-tip').remove();
+        if (option.type != validType) return;
+        self.on('blur',function() {
+          action.common(self, option.reg, false, data.text);
+        });
+      });
+    },
+
+    /**
+     * @function hasError
+     * @description 失焦后校验所有input，一般用在form提交时
+     */
+    hasError: function() {
+      $('input[data-valid]').blur();
+      return $('.valid-error').length;
+    },
+
+    /**
+     * @function init
+     * @description 所有input初始化
+     */
+    init: function() {
+      var inputList = $('input[data-valid]');
+      $('body').on('keyup','input[data-valid]',function(){
+        //输入的时候移除红色警示
+        var self = $(this);
+        self.parent().removeClass('valid-error');
+        self.siblings('.error-tip').remove();
+      });
+
+      inputList.map(function(index, ele) {
+        var input = $(ele),
+          data = eval('(' + input.data('valid') + ')'),
+          validType = data.type,
+          errorText = data.text || '';
+        switch (validType) {
+          case 'validLen':
+            input.validLen(data.min, data.max);
+            break;
+          default:
+            input[validType](errorText);
         }
       });
-    });
+    }
   };
 
-  /**
-   * @function init
-   * @description 所有input初始化
-   */
-  var init =  function() {
-    var inputList = $('input[data-valid]');
-    $('body').on('keyup','input[data-valid]',function(){
-      //输入的时候移除红色警示
-      var self = $(this);
-      self.parent().removeClass('error');
-      self.siblings('.error-tip').remove();
-    });
-
-    inputList.map(function(index, ele) {
-      var input = $(ele),
-        data = eval('(' + input.data('valid') + ')'),
-        validType = data.type,
-        errorText = data.text || '';
-
-      switch (validType) {
-        case 'Null':
-          input.validNull(errorText);
-          break;
-        case 'En':
-          input.validEn(errorText);
-          break;
-        case 'Num':
-          input.validNum(errorText);
-          break;
-        case 'Phone':
-          input.validPhone(errorText);
-          break;
-        case 'Len':
-          input.validLen(data.min, data.max);
-          break;
-      }
-    });
-  };
-
-  return {
-    init: init,
-    register: register
-  }
+  return valid;
 });
